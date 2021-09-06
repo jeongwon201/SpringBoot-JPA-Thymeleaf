cdf330a9a91a918031aa5a5eaf781f89724670a1
jeongwon201 <jeongwon201@naver.com>
2021-09-06 22:30:06 +0900

    @로그인 구현

--- ImageShop/src/main/java/org/hdcd/config/SecurityConfig.java
+++ ImageShop/src/main/java/org/hdcd/config/SecurityConfig.java
@@ -1,28 +1,78 @@
 package org.hdcd.config;
 
+import javax.sql.DataSource;
+
+import org.hdcd.common.security.CustomAccessDeniedHandler;
+import org.hdcd.common.security.CustomLoginSuccessHandler;
+import org.hdcd.common.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.formLogin();
-	}
+	@Autowired
+	DataSource dataSource;
 	
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin()
+			.loginPage("/auth/login")
+			.loginProcessingUrl("/login")
+			.successHandler(authenticationSuccessHandler());
 		
+		http.exceptionHandling()
+			.accessDeniedHandler(accessDeniedHandler());
+		
+		http.rememberMe()
+			.key("hdcd")
+			.tokenRepository(createJDBCRepository())
+			.tokenValiditySeconds(60*60*24);
 	}
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean
+	public UserDetailsService userDetailsSrevice() {
+		return new CustomUserDetailsService();
+	}
+	
+	
+	@Bean
+	public AuthenticationSuccessHandler authenticationSuccessHandler() {
+		return new CustomLoginSuccessHandler();
+	}
+	
+	@Bean
+	public AccessDeniedHandler accessDeniedHandler() {
+		return new CustomAccessDeniedHandler();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService())
+			.passwordEncoder(passwordEncoder());
+	}
+	
+	private PersistentTokenRepository createJDBCRepository() {
+		JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+		repo.setDataSource(dataSource);
+		
+		return repo;
+	}
+	
 }
